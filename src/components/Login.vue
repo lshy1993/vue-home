94b36b6c63ca31393303d945d5a75fe6934e4e7b
lucky <lshy1993@live.com>
2018-10-09 18:33:30 +0900

    login 0.1

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,11 @@
 <template>
 <div class="LoginBox" @click.stop>
     <form ref="loginForm" autocomplete="on" :model="loginForm" :rules="loginRules">
-            <input type="text" v-model="loginForm.email" placeholder="Username" autocomplete="on">
-            <input type="password" v-model="loginForm.password" placeholder="Password" autocomplete="on">
-                <button type="button" @click="handleLogin('loginForm')">
-                    <span>登录</span>
-                </button>
+        <input type="text" v-model="loginForm.email" placeholder="Username" autocomplete="on">
+        <input type="password" v-model="loginForm.password" placeholder="Password" autocomplete="on">
+        <button type="button" @click="handleLogin('loginForm')">
+            <span>登录</span>
+        </button>
         <div class="tips">admin账号为:admin@wz.com 密码123456</div>
         <div class="tips">editor账号:editor@wz.com 密码123456</div>
     </form>
@@ -35,7 +35,21 @@ export default {
 .loginBox {
     width: 400px;
     height: 200px;
+    padding: 20px;
     background: white;
     text-align: center;
+
+    input {
+        display: block;
+        width: 100%;
+        height: 30px;
+        text-align: left;
+        margin-top: 10px;
+    }
+
+    button {
+        margin-top: 10px; 
+        width: 100%;
+    }
 }
 </style>
